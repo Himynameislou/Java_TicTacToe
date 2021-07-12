4fa9f2a07c7c67d5db934964740d2399e8f8a6f3
himynameislou <louie1598@gmail.com>
2021-07-12 16:55:34 -0500

    Set title to top window "Animal Application"

--- Java/javaFXPractice/src/App/Main.java
+++ Java/javaFXPractice/src/App/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../view/MainMenu.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Animal Application");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
