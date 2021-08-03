59f59ce5453b4fc9a750c1fac0589f4f4b19a88f
himynameislou <louie1598@gmail.com>
2021-08-03 14:12:39 -0500

    Added Constructor. Declared Random, JFrame,Jpanel, Button Panel, JLabels, JButton. Methods; firstTurn, checj, xWins, oWins

--- Java/TicTacToe_Java/src/TicTacToe.java
+++ Java/TicTacToe_Java/src/TicTacToe.java
@@ -1,7 +1,42 @@
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class TicTacToe implements ActionListener {
+    Random random = new Random();
+    JFrame frame = new JFrame();
+    JPanel title_panel = new JPanel();
+    JPanel button_panel = new JPanel();
+    JLabel textfield = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean player1_turn;
+
+    /**
+     * Constructor
+     */
+    TicTacToe(){
+
+    }
+
+    //Randomly assigns first turn to user
+    public void firstTurn(){
+
+    }
+
+    //Checks for winning conditions
+    public void check(){
+
+    }
+
+    //Scenario where X Wins
+    public void xWins(int a, int b, int c){
+    }
+
+    //Scenario where O Wins
+    public void oWins(int a, int b, int c){
+    }
+
 
 
     @Override
