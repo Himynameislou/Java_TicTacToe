7e46e16e2b471ea1564fa3d971d5f091dff3acfe
himynameislou <louie1598@gmail.com>
2021-08-03 15:11:37 -0500

    Trouble Shooting fonts not showing up

--- Java/TicTacToe_Java/src/TicTacToe.java
+++ Java/TicTacToe_Java/src/TicTacToe.java
@@ -9,7 +9,7 @@ public class TicTacToe implements ActionListener {
     JFrame frame = new JFrame();
     JPanel title_panel = new JPanel();
     JPanel button_panel = new JPanel();
-    JLabel textfield = new JLabel();
+    JLabel textF = new JLabel();
     JButton[] buttons = new JButton[9];
     boolean player1_turn;
 
@@ -23,12 +23,18 @@ public class TicTacToe implements ActionListener {
         frame.setLayout(new BorderLayout());
         frame.setVisible(true);
 
-        textfield.setBackground(new Color(25,25,25));
-        textfield.setForeground(new Color(25,255,0));
-        textfield.setFont(new Font("Ink Free", font.BOLD, 75));
-        textfield.setHorizontalAlignment(JLabel.CENTER);
-        textfield.setText("Tic-Tac-Toe");
-        textfield.setOpaque(true);
+        textF.setBackground(new Color(25,25,25));
+        textF.setForeground(new Color(25,255,0));
+        textF.setFont(new Font("Ink Free", Font.BOLD, 75));
+        textF.setHorizontalAlignment(JLabel.CENTER);
+        textF.setText("Tic-Tac-Toe");
+        textF.setOpaque(true);
+
+        title_panel.setLayout(new BorderLayout());
+        title_panel.setBounds(0,0,800,100);
+
+        title_panel.add(textF);
+        frame.add(title_panel);
     }
 
     //Randomly assigns first turn to user
