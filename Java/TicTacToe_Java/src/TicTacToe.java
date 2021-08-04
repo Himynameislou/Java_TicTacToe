a5b82ede3cc1c7a5e68c2b836753a31f1c7a908c
himynameislou <louie1598@gmail.com>
2021-08-04 17:41:57 -0500

    Added TTT FXML I may want to add to final

--- Java/TicTacToe_Java/src/TicTacToe.java
+++ Java/TicTacToe_Java/src/TicTacToe.java
@@ -69,6 +69,9 @@ public class TicTacToe implements ActionListener {
 
     //Checks for winning conditions
     public void check(){
+        // check X win conditions
+
+        //check O win conditions
 
     }
 
